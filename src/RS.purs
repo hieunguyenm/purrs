96ce7f4d6b07b2b056a406ce85def2682f1cc121
Hieu Nguyen <hello@hieu.dev>
2020-06-09 18:49:29 +0100

    Add corrupt message check functions

--- src/RS.purs
+++ src/RS.purs
@@ -1,10 +1,14 @@
-module RS (encodeMsg, generator) where
+module RS (check, encodeMsg, generator, syndrome) where
 
 import Prelude
 import Data.Array (length, replicate)
+import Data.Foldable (maximum)
+import Data.FoldableWithIndex (foldWithIndexM)
+import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
 import Effect.Exception.Unsafe (unsafeThrow)
-import Galois (Poly, StateL, gfPolyDiv, gfPolyMul, gfPow)
+import Galois (Poly, StateL, gfPolyDiv, gfPolyEval, gfPolyMul, gfPow)
+import Partial.Unsafe (unsafeCrashWith)
 
 generator :: Int -> StateL Poly
 generator sym = genPoly sym 0 [ 1 ]
@@ -29,3 +33,20 @@ encodeMsg msg sym
     g <- generator sym
     (Tuple _ r) <- gfPolyDiv (msg <> replicate (length g - 1) 0) g
     pure $ msg <> r
+
+syndrome :: Array Int -> Int -> StateL (Array Int)
+syndrome msg sym = foldWithIndexM f [] $ replicate sym 0
+  where
+  f = \i a _ -> do
+    p <- gfPow 2 i
+    v <- gfPolyEval msg p
+    pure $ a <> [ v ]
+
+check :: Array Int -> Int -> StateL Boolean
+check msg sym = do
+  p <- syndrome msg sym
+  pure
+    $ case maximum p of
+        Just 0 -> true
+        Just _ -> false
+        _ -> unsafeCrashWith $ "check: failed to find maximum with p=" <> show p
--- test/RSTest.purs
+++ test/RSTest.purs
@@ -2,8 +2,9 @@ module Test.RS where
 
 import Prelude
 import Control.Monad.State (evalState)
+import Data.Array (drop, replicate)
 import Galois (initLookups)
-import RS (encodeMsg, generator)
+import RS (check, encodeMsg, generator, syndrome)
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 
@@ -12,11 +13,28 @@ spec =
   describe "RS" do
     let
       lkps = initLookups
+
+      plainMsg = [ 0x40, 0xd2, 0x75, 0x47, 0x76, 0x17, 0x32, 0x06, 0x27, 0x26, 0x96, 0xc6, 0xc6, 0x96, 0x70, 0xec ]
+
+      encodedMsg = plainMsg <> [ 0xbc, 0x2a, 0x90, 0x13, 0x6b, 0xaf, 0xef, 0xfd, 0x4b, 0xe0 ]
+
+      corruptMsg = [ 0 ] <> drop 1 encodedMsg
+
+      sym = 10
     it "calculates correct generator polynomials" do
-      a1 <- pure $ flip evalState lkps $ generator 10
-      shouldEqual [ 1, 216, 194, 159, 111, 199, 94, 95, 113, 157, 193 ] a1
-    it "encodes messages correctly" do
+      a1 <- pure $ flip evalState lkps $ generator sym
+      shouldEqual ([ 1, 216, 194, 159, 111, 199, 94, 95, 113, 157, 193 ]) a1
+    it "encodes messages" do
       a1 <-
-        pure $ flip evalState lkps
-          $ encodeMsg [ 0x40, 0xd2, 0x75, 0x47, 0x76, 0x17, 0x32, 0x06, 0x27, 0x26, 0x96, 0xc6, 0xc6, 0x96, 0x70, 0xec ] 10
-      shouldEqual [ 0x40, 0xd2, 0x75, 0x47, 0x76, 0x17, 0x32, 0x6, 0x27, 0x26, 0x96, 0xc6, 0xc6, 0x96, 0x70, 0xec, 0xbc, 0x2a, 0x90, 0x13, 0x6b, 0xaf, 0xef, 0xfd, 0x4b, 0xe0 ] a1
+        pure $ flip evalState lkps $ encodeMsg plainMsg sym
+      shouldEqual encodedMsg a1
+    it "calculates correct syndrome polynomials" do
+      a1 <- pure $ flip evalState lkps $ syndrome encodedMsg sym
+      shouldEqual a1 $ replicate sym 0
+      a2 <- pure $ flip evalState lkps $ syndrome corruptMsg sym
+      shouldEqual [ 64, 192, 93, 231, 52, 92, 228, 49, 83, 245 ] a2
+    it "checks for corrupt messages correctly" do
+      a1 <- pure $ flip evalState lkps $ check encodedMsg sym
+      shouldEqual true a1
+      a2 <- pure $ flip evalState lkps $ check corruptMsg sym
+      shouldEqual false a2
